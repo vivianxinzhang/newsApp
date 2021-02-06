0a6db3e9977f86513d624fe43598914627c0bba7
Vivian Zhang <vivianxinzhang@gmail.com>
2021-02-06 00:50:51 -0800

    Search Page Implementation using Recycler View

--- app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
@@ -1,23 +1,26 @@
 package com.laioffer.tinnews.ui.search;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.laioffer.tinnews.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.laioffer.tinnews.databinding.FragmentSearchBinding;
 import com.laioffer.tinnews.repository.NewsRepository;
 import com.laioffer.tinnews.repository.NewsViewModelFactory;
-import com.laioffer.tinnews.ui.home.HomeViewModel;
 
 public class SearchFragment extends Fragment {
+
+    private SearchViewModel viewModel;
+    private FragmentSearchBinding binding;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -27,22 +30,58 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        // return inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Now we have a reference to the binding. We do not need to use findViewById for each view.
+        // Any views with an @+id tag will have a binding automatically.
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
 
+        SearchNewsAdapter newsAdapter = new SearchNewsAdapter();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(requireContext(), 2);
+        binding.newsResultsRecyclerView.setLayoutManager(gridLayoutManager);
+        binding.newsResultsRecyclerView.setAdapter(newsAdapter);
+
+        // In SearchFragment, add a OnQueryTextListener to the search view.
+        // We set the search input in the onQueryTextSubmit callback.
+        binding.newsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!query.isEmpty()) {
+                    viewModel.setSearchInput(query);
+                }
+                binding.newsSearchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         NewsRepository repository = new NewsRepository(getContext());
-        SearchViewModel viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
                 .get(SearchViewModel.class);
-        viewModel.setSearchInput("Covid-19");
+
+
+        // viewModel.setSearchInput("Covid-19");
         viewModel.searchNews()
                 .observe(getViewLifecycleOwner(), newsResponse -> {
                     if (newsResponse != null) {
                         Log.d("SearchFragment", newsResponse.toString());
+                        newsAdapter.setArticles(newsResponse.articles);
                     }
                 });
+
+
+
     }
 }
